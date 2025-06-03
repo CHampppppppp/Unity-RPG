f08f4a010ca0951ec1a4ba8c01b7e0fb39e08da4
champ <1handsome3206@gmail.com>
2025-06-03 10:47:08 +0800

    sword return

--- scripts/Enemy/Enemy.cs
+++ scripts/Enemy/Enemy.cs
@@ -72,4 +72,5 @@ public class Enemy : Entity
         return false;
 
     }
+
 }
---
+++ scripts/Skills/Controllers/BlackHoleController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleController : MonoBehaviour
+{
+    public float maxSize;
+    public float growSpeed;
+    public bool canGrow;
+
+    public List<Transform> targets;
+
+
+    private void Update()
+    {
+        if (canGrow)
+            transform.localScale = Vector2.Lerp(transform.localScale, new Vector2(maxSize , maxSize),growSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Enemy>() != null)
+        {
+            collision.GetComponent<Enemy>().enabled = false;
+
+            //targets.Add(collision.transform);
+        }
+    }
+}
